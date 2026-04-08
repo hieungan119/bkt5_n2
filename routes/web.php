fdb3fb3578029f6392a75582de43d6be24887e27
NhienTran133 <hiennhien130305@gmail.com>
2026-04-08 14:51:49 +0700

    Chi tiếtê

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,3 @@ Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
 
 
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
-Route::get('/home','App\Http\Controllers\MovieControllerNhien@index');
-Route::get('home/detail/{id}','App\Http\Controllers\MovieControllerNhien@detail');
