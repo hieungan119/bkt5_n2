202ce274e08c3ce7b83d60aaa05bb130e76a2cbd
hieungan119 <030239230139@st.buh.edu.vn>
2026-04-08 15:56:54 +0700

    Restore missing files from DangPhuongNghi

---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,19 @@
+<x-movie-layout>
+    <x-slot name="title">
+        Movie
+    </x-slot>
+
+    <div class="list-movie">
+        @foreach($data as $row)
+            <div class="movie">
+                <a href="#">
+                    <img src="{{ asset('storage' . $row->image) }}" width="200" height="300">
+                    <br>
+                    <b>{{ $row->movie_name_vn }}</b>
+                    <br>
+                    <b>{{ $row->release_date }}</b>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</x-movie-layout>
