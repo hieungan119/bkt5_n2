318bbfa3a549580464c9efe611145d87def424a4
NhienTran133 <hiennhien130305@gmail.com>
2026-04-08 15:10:46 +0700

    Nhiên

--- resources/views/movie/index.blade.php
+++ resources/views/movie/index.blade.php
@@ -2,15 +2,4 @@
     <x-slot name="title">
         Movie
     </x-slot>
-    <div class='list-movie'>
-        @foreach($data as $row)
-            <div class='movie'>
-                <a href="{{ url('home/detail/' . $row->id) }}">
-                <img src="{{$row->image_link}}" width='200px' height='300px'><br>
-                <b>{{$row->movie_name_vn}}</b><br/>
-                <b>{{$row->release_date}}</b><br/>
-                </a>
-            </div>
-        @endforeach
-    </div>
 </x-movie-layout>
