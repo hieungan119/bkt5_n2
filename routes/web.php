864f74466d9455bbc274a9a42bf2b40b72c30f5d
minnwu-1205 <nhuminh12005@gmail.com>
2026-04-08 14:43:57 +0700

    Mnhu

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\MovieController4;
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
 
 Route::get('/', [MovieController4::class, 'index']);
+
 Route::get('/theloai/{id}', [MovieController4::class, 'genre']);
+
 Route::post('/timkiem', [MovieController4::class, 'search'])->name('search');
+
 Route::get('/phim/{id}', [MovieController4::class, 'show']);
