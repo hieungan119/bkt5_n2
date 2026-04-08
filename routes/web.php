bba002d52f7b12bd62267ad2261397d25c7b77fd
hieungan119 <030239230139@st.buh.edu.vn>
2026-04-08 15:04:16 +0700

    ngan

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenRouterController;
+use App\Http\Controllers\AddMovieController;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
-
+Route::get('/test', function(){
+    phpinfo();
+});
 
 
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
+Route::get('/movie/admin/create', [AddMovieController::class, 'create'])->name('admin.create');
+Route::post('movie/admin/store', [AddMovieController::class, 'store'])->name('admin.store');
